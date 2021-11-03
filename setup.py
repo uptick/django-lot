79070b54dc4a8faad8a77d8221b12553c4e9ff0a
william chu <william.chu@uptickhq.com>
2021-11-03 18:55:16 +1100

    Make django-lot version number PEP440 compatible

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url = "https://github.com/jespino/django-lot",
     author = "Jesús Espino, Curtis Malone",
     author_email = "jespinog@gmail.com, curtis@tinbrain.net",
-    version='v0.2.2-uptick',
+    version='v0.2.2.1',
     packages = [
         "lot",
         "lot.migrations",
