f84ddc468f0a9b768ecf48d807cf71a9063dc1d5
Jarek Głowacki <jarekwg@gmail.com>
2022-11-14 12:59:06 +1100

    Update urls to more modern django style.

--- lot/urls.py
+++ lot/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r"^login/(?P<uuid>[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12})/$", views.LOTLogin.as_view(), name="login"),
+    path("login/<uuid:uuid>/", views.LOTLogin.as_view(), name="login"),
 ]
