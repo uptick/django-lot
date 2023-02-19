6efcde15e4b50bcd32463b27dcfdb7b500baf3a4
Paul Hutchins <paul.hutchins@uptickhq.com>
2023-02-20 10:41:12 +1100

    Fix: Update django.conf.url imports inline with django40Removal

--- tests/urls.py
+++ tests/urls.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, include, url
+from django.urls import include, patterns, re_path
 
 from .tests import TestView
 
-
-urlpatterns = patterns("",
-    url(r"", include('lot.urls')),
-    url(r"^test_url/$", TestView.as_view()),
+urlpatterns = patterns(
+    "",
+    re_path(r"", include("lot.urls")),
+    re_path(r"^test_url/$", TestView.as_view()),
 )
