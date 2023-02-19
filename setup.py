277c349a431de7fafabfe6a968759aeb79512dbb
Paul Hutchins <paul.hutchins@uptickhq.com>
2023-02-20 10:53:47 +1100

    Version(0.2.5): Change version to 0.2.5 and rebundle with wheels

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url = "https://github.com/jespino/django-lot",
     author = "Jesús Espino, Curtis Malone",
     author_email = "jespinog@gmail.com, curtis@tinbrain.net",
-    version='0.2.4',
+    version='0.2.5',
     packages = [
         "lot",
         "lot.migrations",
